016bf01d08551239adb120d01732d00ad6134358
iam-dasari <dasaridevops2025@gmail.com>
2025-07-26 12:57:04 +0530

    Git Repository BAckup

--- git_repository_backup.sh
+++ git_repository_backup.sh
@@ -2,3 +2,4 @@
 git_repo="https://github.com/iam-dasari/shell-script-for-devops.git"
 backup_dir="/tmp/git_backup"
 git clone --mirror "$git_repo" "$backup_dir/shell-script-for-devops.git"
+echo "Git Clone Done"
